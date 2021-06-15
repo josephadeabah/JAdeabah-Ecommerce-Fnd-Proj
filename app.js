b14b2029ed459e6ec812c9666085d7acbddd6e12
josephadeabah <joseph.adeabah@amalitech.org>
2021-06-15 12:57:15 +0000

    reduced the margin top for the coupon box

--- app.js
+++ app.js
@@ -276,3 +276,16 @@ function op()
 {
 document.getElementById('imageId').innerHTML=backgroundImageUrl;
 }
+
+
+$(document).ready(function() {
+
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+
+});
